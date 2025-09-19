1671bb63836b411575e92e1c165d6746a5895346
Chuseok22 <bjh59629@naver.com>
2025-09-19 11:09:40 +0900

    spring boot init

---
+++ src/main/java/com/chuseok22/stocktrader/StockTraderApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.stocktrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTraderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StockTraderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chuseok22/stocktrader/StockTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.stocktrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTraderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
