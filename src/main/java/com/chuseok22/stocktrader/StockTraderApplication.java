7a931cc8efd03505e66c6054701492675d8f5453
Chuseok22 <bjh59629@naver.com>
2025-09-19 12:36:32 +0900

    데이터베이스_설계 : feat : JPA Auditing 추가 https://github.com/Chuseok22/stock-trader/issues/2

--- src/main/java/com/chuseok22/stocktrader/StockTraderApplication.java
+++ src/main/java/com/chuseok22/stocktrader/StockTraderApplication.java
@@ -2,8 +2,10 @@ package com.chuseok22.stocktrader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StockTraderApplication {
 
   public static void main(String[] args) {
